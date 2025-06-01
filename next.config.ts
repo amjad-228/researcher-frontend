d67d943e7baf9b2947bb554c2d0c8219e2829003
family-tree-alhaj-ali <sathhat.ramadan.jeddah@gmail.com>
2025-06-01 07:49:55 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // ✅ هذا يعطل الأخطاء عند البناء في Vercel
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
   reactStrictMode: true,
 };
